cfdaa3fa9ddd2f43b6fe83f4abcedff3cf90233b
Ben Barnard <barnardb@gmail.com>
2020-04-17 22:05:17 +0200

    GeoJsonStyle readability

--- core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/GeoJsonStyle.scala
+++ core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/GeoJsonStyle.scala
@@ -1,6 +1,5 @@
 package berlin.softwaretechnik.geojsonrenderer.geojson
 
-import ujson.{Num, Str}
 import ujson.Value.Value
 
 case class GeoJsonStyle(
@@ -15,42 +14,17 @@ case class GeoJsonStyle(
 
 object GeoJsonStyle {
   def apply(properties: Map[String, Value]): GeoJsonStyle = {
+    def lookup[A](key: String, value: Value => Option[A], default: A): A =
+      properties.get(key).flatMap(value).getOrElse(default)
+
     GeoJsonStyle(
-      properties
-        .collectFirst {
-          case ("stroke", Str(stroke)) => stroke
-        }
-        .getOrElse("#555555"),
-      properties
-        .collectFirst {
-          case ("stroke-opacity", Num(num)) => num.toString
-        }
-        .getOrElse("1"),
-      properties
-        .collectFirst {
-          case ("fill", Str(fill)) => fill
-        }
-        .getOrElse("#555555"),
-      properties
-        .collectFirst {
-          case ("fill-opacity", Num(num)) => num.toString
-        }
-        .getOrElse("0.6"),
-      properties
-        .collectFirst {
-          case ("stroke-width", Num(strokeWidth)) => strokeWidth.toString
-        }
-        .getOrElse("3"),
-      properties
-        .collectFirst {
-          case ("title", Num(strokeWidth)) => strokeWidth.toString
-        }
-        .getOrElse(""),
-      properties
-        .collectFirst {
-          case ("description", Num(strokeWidth)) => strokeWidth.toString
-        }
-        .getOrElse("")
+      lookup("stroke", _.strOpt, "#555555"),
+      lookup("stroke-opacity", _.numOpt, 1).toString,
+      lookup("fill", _.strOpt, "#555555"),
+      lookup("fill-opacity", _.numOpt, 0.6).toString,
+      lookup("stroke-width", _.numOpt, 3).toString,
+      lookup("title", _.strOpt, ""),
+      lookup("description", _.strOpt, ""),
     )
   }
 }
