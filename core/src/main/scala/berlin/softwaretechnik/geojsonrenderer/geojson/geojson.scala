20aad33fe219fb3d596d8cec084f8852773987a9
Ben Barnard <barnardb@gmail.com>
2020-04-17 15:44:04 +0200

    Make upickle typeclass instances package-private

--- core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/geojson.scala
+++ core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/geojson.scala
@@ -5,7 +5,7 @@ import java.io.File
 
 import ujson.Value.Value
 
-object json extends upickle.AttributeTagged {
+private object json extends upickle.AttributeTagged {
   override def tagName: String = "type"
 }
 
@@ -15,65 +15,62 @@ case class FeatureCollection(features: Seq[Feature])
 
 @upickle.implicits.key("FeatureCollection")
 object FeatureCollection {
-  implicit val rw: ReadWriter[FeatureCollection] = macroRW
+  private[geojson] implicit val rw: ReadWriter[FeatureCollection] = macroRW
 }
 
 @upickle.implicits.key("Feature")
 case class Feature(geometry: Geometry, properties: Map[String, Value])
 
 object Feature {
-  implicit val rw: ReadWriter[Feature] = macroRW
+  private[geojson] implicit val rw: ReadWriter[Feature] = macroRW
 }
 
 sealed trait Geometry
 
 object Geometry {
-  implicit val rw: ReadWriter[Geometry] = macroRW
-
+  private[geojson] implicit val rw: ReadWriter[Geometry] = macroRW
 }
 
 @upickle.implicits.key("Point")
 sealed case class Point(coordinates: GeoCoord) extends Geometry
 
 object Point {
-  implicit val rw: ReadWriter[Point] = macroRW
-
+  private[geojson] implicit val rw: ReadWriter[Point] = macroRW
 }
 
 @upickle.implicits.key("MultiPoint")
 sealed case class MultiPoint(coordinates: Seq[GeoCoord]) extends Geometry
 
 object MultiPoint {
-  implicit val rw: ReadWriter[MultiPoint] = macroRW
-
+  private[geojson] implicit val rw: ReadWriter[MultiPoint] = macroRW
 }
 
 @upickle.implicits.key("LineString")
 sealed case class LineString(coordinates: Seq[GeoCoord]) extends Geometry
 
 object LineString {
-  implicit val rw: ReadWriter[LineString] = macroRW
+  private[geojson] implicit val rw: ReadWriter[LineString] = macroRW
 }
 
 @upickle.implicits.key("MultiLineString")
 sealed case class MultiLineString(coordinates: Seq[Seq[GeoCoord]]) extends Geometry
 
 object MultiLineString {
-  implicit val rw: ReadWriter[MultiLineString] = macroRW
+  private[geojson] implicit val rw: ReadWriter[MultiLineString] = macroRW
 }
 
 @upickle.implicits.key("Polygon")
 sealed case class Polygon(coordinates: Seq[Seq[GeoCoord]]) extends Geometry
 
 object Polygon {
-  implicit val rw: ReadWriter[Polygon] = macroRW
+  private[geojson] implicit val rw: ReadWriter[Polygon] = macroRW
 }
 
 @upickle.implicits.key("MultiPolygon")
 sealed case class MultiPolygon(coordinates: Seq[Seq[Seq[GeoCoord]]]) extends Geometry
 
 object MultiPolygon {
-  implicit val rw: ReadWriter[MultiPolygon] = macroRW
+  private[geojson] implicit val rw: ReadWriter[MultiPolygon] = macroRW
 }
 
 
--- core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/package.scala
+++ core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/package.scala
@@ -1,6 +1,6 @@
 package berlin.softwaretechnik.geojsonrenderer
 
 package object geojson {
-  implicit val geoCoordReadWriter: json.ReadWriter[GeoCoord] =
+  private[geojson] implicit val geoCoordReadWriter: json.ReadWriter[GeoCoord] =
     json.readwriter[Seq[Double]].bimap[GeoCoord](gc => Seq(gc.lon, gc.lat), cs => GeoCoord(cs(1), cs(0)))
 }
