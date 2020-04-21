440d53af84d699760284d5675c3a3d1a2397447d
Ben Barnard <barnardb@gmail.com>
2020-04-21 22:20:07 +0200

    Simplify GeoJsonStyle

--- core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/GeoJsonStyle.scala
+++ core/src/main/scala/berlin/softwaretechnik/geojsonrenderer/geojson/GeoJsonStyle.scala
@@ -3,33 +3,19 @@ package berlin.softwaretechnik.geojsonrenderer.geojson
 import ujson.Value.Value
 
 /**
- * GeoJSON style, derived from commonly used properties.
+ * Extracts style and other values from commonly used GeoJSON properties.
  *
  * @see https://github.com/mapbox/simplestyle-spec/tree/master/1.1.0
  */
-case class GeoJsonStyle(
-  stroke: String,
-  strokeOpacity: Double,
-  fill: String,
-  fillOpacity: Double,
-  strokeWidth: Double,
-  title: String,
-  description: String
-)
+case class GeoJsonStyle(properties: Map[String, Value]) {
+  def stroke: String = lookup("stroke", _.strOpt, "#555555")
+  def strokeOpacity: Double = lookup("stroke-opacity", _.numOpt, 1)
+  def fill: String = lookup("fill", _.strOpt, "#555555")
+  def fillOpacity: Double = lookup("fill-opacity", _.numOpt, 0.6)
+  def strokeWidth: Double = lookup("stroke-width", _.numOpt, 3)
+  def title: String = lookup("title", _.strOpt, "")
+  def description: String = lookup("description", _.strOpt, "")
 
-object GeoJsonStyle {
-  def apply(properties: Map[String, Value]): GeoJsonStyle = {
-    def lookup[A](key: String, value: Value => Option[A], default: A): A =
-      properties.get(key).flatMap(value).getOrElse(default)
-
-    GeoJsonStyle(
-      lookup("stroke", _.strOpt, "#555555"),
-      lookup("stroke-opacity", _.numOpt, 1),
-      lookup("fill", _.strOpt, "#555555"),
-      lookup("fill-opacity", _.numOpt, 0.6),
-      lookup("stroke-width", _.numOpt, 3),
-      lookup("title", _.strOpt, ""),
-      lookup("description", _.strOpt, ""),
-    )
-  }
+  private def lookup[A](key: String, value: Value => Option[A], default: A): A =
+    properties.get(key).flatMap(value).getOrElse(default)
 }
