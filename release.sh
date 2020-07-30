dce419e4c920233c09780d11b778a775e8c98be1
Felix Leipold <felix.leipold@gmail.com>
2020-07-30 13:30:15 +0200

    Harden release script

--- release.sh
+++ release.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+# This script pushes to maven central.
+#
+# For authentication it relies on a nubmer of environment variables:
+# SONATYPE_USER: User used for the sonatype nexus instance.
+# SONATYPE_PASSWORD: Password used for the sonatype nexus instance.
+# GPG_KEY: The gpg key to be used to sign the artifacts.
+# GPG_PASSPHRASE: Passphrase for the gpg key.
+
+if [[ ! -v SONATYPE_USER ]]; then
+   echo SONATYPE_USER needs to be specified in environment. >&2
+fi
+
+if [[ ! -v SONATYPE_PASSWORD ]]; then
+   echo SONATYPE_PASSWORD needs to be specified in environment. >&2
+fi
+
+if [[ ! -v GPG_KEY ]]; then
+   echo GPG_KEY needs to be specified in environment. >&2
+fi
+
+if [[ ! -v GPG_PASSPHRASE ]]; then
+   echo GPG_PASSPHRASE needs to be specified in environment. >&2
+fi
 
 set -e
 set -u
